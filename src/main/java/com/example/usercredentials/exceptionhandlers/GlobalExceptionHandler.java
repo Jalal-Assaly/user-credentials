0b81c3e2529e143fac4f1824a59899004eebe7eb
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-04 18:09:33 +0200

    Add Global Exception Handler
    Signed-off-by: Jalal Assaly <jalal.assaly@gmail.com>

---
+++ src/main/java/com/example/usercredentials/exceptionhandlers/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.usercredentials.exceptionhandlers;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponseBody> handleConstraintViolationException(ConstraintViolationException exception) {
+        HttpStatusCode status = HttpStatus.BAD_REQUEST;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(status, exception);
+        return new ResponseEntity<>(exceptionResponseBody, status);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionResponseBody> handleEntityNotFoundException(EntityNotFoundException exception) {
+        HttpStatusCode status = HttpStatus.BAD_REQUEST;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(status, exception);
+        return new ResponseEntity<>(exceptionResponseBody, status);
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<ExceptionResponseBody> handleEntityExistsException(EntityExistsException exception) {
+        HttpStatusCode status = HttpStatus.BAD_REQUEST;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(status, exception);
+        return new ResponseEntity<>(exceptionResponseBody, status);
+    }
+}
