969aec2b1d0a0dfef57dd22b4f59a7f504dd297a
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-18 22:32:21 +0200

    Remove unused annotation
    Signed-off-by: Jalal Assaly <jalal.assaly@gmail.com>

--- src/main/java/com/example/usercredentials/UserCredentialsApplication.java
+++ src/main/java/com/example/usercredentials/UserCredentialsApplication.java
@@ -2,10 +2,8 @@ package com.example.usercredentials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class UserCredentialsApplication {
 
     public static void main(String[] args) {
