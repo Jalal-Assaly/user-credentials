0547aeec62dd65f46010e07f50df5d8b090eacc3
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-20 02:10:57 +0200

    Remove service interface
    Signed-off-by: Jalal Assaly <jalal.assaly@gmail.com>

--- src/main/java/com/example/usercredentials/services/UserService.java
+++ src/main/java/com/example/usercredentials/services/UserService.java
@@ -1,17 +1,66 @@
 package com.example.usercredentials.services;
 
+import com.example.usercredentials.documents.User;
 import com.example.usercredentials.models.UserModel;
+import com.example.usercredentials.models.mappers.UserMapper;
+import com.example.usercredentials.repositories.UserRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
+@Service
 @Validated
-public interface UserService {
-    List<UserModel> getAllUsers();
-    UserModel findUserById(String id);
-    UserModel findUserBySsn(String SSN);
-    void addUser(@Valid UserModel userModel);
-    void deleteUser(String id);
-    void updateUser(String id, @Valid UserModel userModel);
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public List<UserModel> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        return users.stream()
+                .map(userMapper::toUserModel)
+                .toList();
+    }
+
+    public UserModel findUserById(String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User was not found"));
+        return userMapper.toUserModel(user);
+    }
+
+    public UserModel findUserBySsn(String ssn) {
+        User user = userRepository.findUserBySsn(ssn)
+                .orElseThrow(() -> new EntityNotFoundException("User was not found"));
+        return userMapper.toUserModel(user);
+    }
+
+    public void addUser(@Valid UserModel userModel) {
+        User user = userMapper.toUser(userModel);
+        if(userRepository.existsById(user.getId())) {
+            throw new EntityExistsException("User already exists");
+        } else {
+            userRepository.save(user);
+        }
+    }
+
+    public void deleteUser(String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User was not found"));
+        userRepository.delete(user);
+    }
+
+    public void updateUser(String id, @Valid UserModel userModel) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User was not found"));
+        BeanUtils.copyProperties(userModel, user, "id");
+        userRepository.save(user);
+    }
 }
