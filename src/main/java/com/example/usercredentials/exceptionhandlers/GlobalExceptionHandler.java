7ea7889991c6d73186379206402830595761b85a
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-27 02:11:37 +0200

    Add custom exception response bodies
    Signed-off-by: Jalal Assaly <jalal.assaly@gmail.com>

--- src/main/java/com/example/usercredentials/exceptionhandlers/GlobalExceptionHandler.java
+++ src/main/java/com/example/usercredentials/exceptionhandlers/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.example.usercredentials.exceptionhandlers;
 
+import com.example.usercredentials.exceptionhandlers.responsebodies.ConstraintViolationExceptionResponseBody;
+import com.example.usercredentials.exceptionhandlers.responsebodies.EntityNotFoundExceptionResponseBody;
+import com.example.usercredentials.exceptionhandlers.responsebodies.ValidationExceptionBody;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +25,23 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ExceptionResponseBody> handleEntityNotFoundException(EntityNotFoundException exception) {
+    public ResponseEntity<EntityNotFoundExceptionResponseBody> handleEntityNotFoundException(EntityNotFoundException exception) {
         HttpStatusCode status = HttpStatus.BAD_REQUEST;
-        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(status, exception);
-        return new ResponseEntity<>(exceptionResponseBody, status);
+        EntityNotFoundExceptionResponseBody entityNotFoundExceptionResponseBody = new EntityNotFoundExceptionResponseBody(status, exception);
+        return new ResponseEntity<>(entityNotFoundExceptionResponseBody, status);
     }
 
     @ExceptionHandler(EntityExistsException.class)
-    public ResponseEntity<ExceptionResponseBody> handleEntityExistsException(EntityExistsException exception) {
+    public ResponseEntity<EntityNotFoundExceptionResponseBody> handleEntityExistsException(EntityExistsException exception) {
         HttpStatusCode status = HttpStatus.BAD_REQUEST;
-        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(status, exception);
-        return new ResponseEntity<>(exceptionResponseBody, status);
+        EntityNotFoundExceptionResponseBody entityNotFoundExceptionResponseBody = new EntityNotFoundExceptionResponseBody(status, exception);
+        return new ResponseEntity<>(entityNotFoundExceptionResponseBody, status);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ValidationExceptionBody> handleValidationException(ValidationException exception) {
+        HttpStatusCode status = HttpStatus.BAD_REQUEST;
+        ValidationExceptionBody body = new ValidationExceptionBody(status,exception);
+        return new ResponseEntity<>(body,status);
     }
 }
