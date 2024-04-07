3be8852c7b82bf8c82118f761f5561a504d50e2f
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-04-07 23:16:13 +0200

    Code refactor

--- src/main/java/org/pacs/usercredentialsapi/services/UserCredentialsService.java
+++ src/main/java/org/pacs/usercredentialsapi/services/UserCredentialsService.java
@@ -13,10 +13,12 @@ import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Service
+@Validated
 @RequiredArgsConstructor
 public class UserCredentialsService {
 
