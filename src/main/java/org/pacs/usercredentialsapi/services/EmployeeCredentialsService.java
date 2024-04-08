8774366a7f099b9b7c5713321e0af78afaacae64
Jalal Assaly <jalal.assaly@gmail.com>
2024-04-08 23:34:18 +0200

    Renamed UserCredentialsServiceTest to EmployeeCredentialsServiceTest
    Signed-off-by: Jalal Assaly <jalal.assaly@gmail.com>

--- src/main/java/org/pacs/usercredentialsapi/services/EmployeeCredentialsService.java
+++ src/main/java/org/pacs/usercredentialsapi/services/EmployeeCredentialsService.java
@@ -1,11 +1,9 @@
 package org.pacs.usercredentialsapi.services;
 
 import org.pacs.usercredentialsapi.documents.EmployeeCredentials;
-import org.pacs.usercredentialsapi.documents.VisitorCredentials;
 import org.pacs.usercredentialsapi.models.UserCredentialsModel;
 import org.pacs.usercredentialsapi.models.mappers.UserMapper;
 import org.pacs.usercredentialsapi.repositories.EmployeeCredentialsRepository;
-import org.pacs.usercredentialsapi.repositories.VisitorCredentialsRepository;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
@@ -20,21 +18,14 @@ import java.util.List;
 @Service
 @Validated
 @RequiredArgsConstructor
-public class UserCredentialsService {
+public class EmployeeCredentialsService {
 
     private final EmployeeCredentialsRepository employeeCredentialsRepository;
-    private final VisitorCredentialsRepository visitorRepository;
     private final UserMapper userMapper;
 
     public List<UserCredentialsModel> getAllEmployees() {
         return employeeCredentialsRepository.findAll()
                 .stream()
-                .map(userMapper::toUserModel)
-                .toList();
-    }
-
-    public List<UserCredentialsModel> getAllVisitors() {
-        return visitorRepository.findAll().stream()
                 .map(userMapper::toUserModel)
                 .toList();
     }
@@ -45,22 +36,10 @@ public class UserCredentialsService {
         return userMapper.toUserModel(employeeCredentials);
     }
 
-    public UserCredentialsModel findVisitorById(String id) {
-        VisitorCredentials visitorCredentials = visitorRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Visitor was not found"));
-        return userMapper.toUserModel(visitorCredentials);
-    }
-
     public UserCredentialsModel findEmployeeByEmail(String email) {
         EmployeeCredentials employeeCredentials = employeeCredentialsRepository.findEmployeeByEmail(email)
                 .orElseThrow(() -> new EntityNotFoundException("Employee was not found"));
         return userMapper.toUserModel(employeeCredentials);
-    }
-
-    public UserCredentialsModel findVisitorByEmail(String email) {
-        VisitorCredentials visitorCredentials = visitorRepository.findVisitorByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("Visitor was not found"));
-        return userMapper.toUserModel(visitorCredentials);
     }
 
     public void addNewEmployee(@Valid UserCredentialsModel employeeModel) {
@@ -72,25 +51,10 @@ public class UserCredentialsService {
         }
     }
 
-    public void addNewVisitor(@Valid UserCredentialsModel visitorModel) {
-        VisitorCredentials visitorCredentials = userMapper.toVisitor(visitorModel);
-        if(visitorRepository.existsById(visitorCredentials.getId())) {
-            throw new EntityExistsException("Visitor already exists");
-        } else {
-            visitorRepository.save(visitorCredentials);
-        }
-    }
-
     public void deleteEmployee(String id) {
         EmployeeCredentials employeeCredentials = employeeCredentialsRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Employee was not found"));
         employeeCredentialsRepository.delete(employeeCredentials);
-    }
-
-    public void deleteVisitor(String id) {
-        VisitorCredentials visitorCredentials = visitorRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Visitor was not found"));
-        visitorRepository.delete(visitorCredentials);
     }
 
     public void updateEmployee(String id, @Valid UserCredentialsModel employeeModel) {
@@ -99,13 +63,5 @@ public class UserCredentialsService {
                 .orElseThrow(() -> new EntityNotFoundException("Employee was not found"));
         BeanUtils.copyProperties(employeeModel, employeeCredentials, "id");
         employeeCredentialsRepository.save(employeeCredentials);
-    }
-
-    public void updateVisitor(String id, @Valid UserCredentialsModel visitorModel) {
-        if(!id.equals(visitorModel.getId())) throw new ValidationException("Path ID and Request ID not matching");
-        VisitorCredentials visitorCredentials = visitorRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Visitor was not found"));
-        BeanUtils.copyProperties(visitorModel, visitorCredentials, "id");
-        visitorRepository.save(visitorCredentials);
     }
 }
--- src/test/java/org/pacs/usercredentialsapi/EmployeeCredentialsServiceTest.java
+++ src/test/java/org/pacs/usercredentialsapi/EmployeeCredentialsServiceTest.java
@@ -1,11 +1,13 @@
 package org.pacs.usercredentialsapi;
 
+import jakarta.validation.ValidationException;
 import org.pacs.usercredentialsapi.documents.EmployeeCredentials;
 
+import org.pacs.usercredentialsapi.documents.VisitorCredentials;
 import org.pacs.usercredentialsapi.models.UserCredentialsModel;
 import org.pacs.usercredentialsapi.models.mappers.UserMapper;
 import org.pacs.usercredentialsapi.repositories.EmployeeCredentialsRepository;
-import org.pacs.usercredentialsapi.services.UserCredentialsService;
+import org.pacs.usercredentialsapi.services.EmployeeCredentialsService;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,38 +25,38 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserCredentialsServiceTest {
+public class EmployeeCredentialsServiceTest {
 
     @Mock
     private EmployeeCredentialsRepository employeeCredentialsRepository;
     @Mock
     private UserMapper userMapper;
     @InjectMocks
-    private UserCredentialsService employeeService;
+    private EmployeeCredentialsService employeeService;
 
     // Some properties
-    EmployeeCredentials testEmployee1Credentials;
-    EmployeeCredentials testEmployee2Credentials;
+    EmployeeCredentials testEmployee1;
+    EmployeeCredentials testEmployee2;
     List<EmployeeCredentials> testEmployeeCredentials;
     UserCredentialsModel testEmployeeModel1;
     UserCredentialsModel testEmployeeModel2;
 
     @BeforeEach void init() {
         // Initial Setup - Employee Entities
-        testEmployee1Credentials = new EmployeeCredentials("3", "432-33-3425", "Mike", "Johns", "mike.johns@example.com", "test234");
-        testEmployee2Credentials = new EmployeeCredentials("53", "543-93-5322", "Laury", "Parker", "laury.parker@example.com", "trialError432");
+        testEmployee1 = new EmployeeCredentials("3", "432-33-3425", "Mike", "Johns", "mike.johns@example.com", "test234");
+        testEmployee2 = new EmployeeCredentials("53", "543-93-5322", "Laury", "Parker", "laury.parker@example.com", "trialError432");
         // Initial Setup - Employee Models
         testEmployeeModel1 = new UserCredentialsModel("3", "432-33-3425", "Mike", "Johns", "mike.johns@example.com", "test234");
         testEmployeeModel2 = new UserCredentialsModel("53", "543-93-5322", "Laury", "Parker", "laury.parker@example.com", "trialError432");
         // Initial Setup - Employees List
-        testEmployeeCredentials = Arrays.asList(testEmployee1Credentials, testEmployee2Credentials);
+        testEmployeeCredentials = Arrays.asList(testEmployee1, testEmployee2);
     }
 
     @Test void testGetAllEmployees() {
         // Arrange
         when(employeeCredentialsRepository.findAll()).thenReturn(testEmployeeCredentials);
-        when(userMapper.toUserModel(testEmployee1Credentials)).thenReturn(testEmployeeModel1);
-        when(userMapper.toUserModel(testEmployee2Credentials)).thenReturn(testEmployeeModel2);
+        when(userMapper.toUserModel(testEmployee1)).thenReturn(testEmployeeModel1);
+        when(userMapper.toUserModel(testEmployee2)).thenReturn(testEmployeeModel2);
 
         // Act
         List<UserCredentialsModel> actualEmployees = employeeService.getAllEmployees();
@@ -69,11 +71,11 @@ public class UserCredentialsServiceTest {
 
     @Test void testFindEmployeeById_Success() {
         // Arrange
-        when(employeeCredentialsRepository.findById(testEmployee1Credentials.getId())).thenReturn(Optional.of(testEmployee1Credentials));
-        when(userMapper.toUserModel(testEmployee1Credentials)).thenReturn(testEmployeeModel1);
+        when(employeeCredentialsRepository.findById(testEmployee1.getId())).thenReturn(Optional.of(testEmployee1));
+        when(userMapper.toUserModel(testEmployee1)).thenReturn(testEmployeeModel1);
 
         // Act
-        UserCredentialsModel actualEmployee = employeeService.findEmployeeById(testEmployee1Credentials.getId());
+        UserCredentialsModel actualEmployee = employeeService.findEmployeeById(testEmployee1.getId());
 
         // Verify & Assert
         verify(employeeCredentialsRepository).findById(any(String.class));
@@ -91,13 +93,13 @@ public class UserCredentialsServiceTest {
                 .hasMessage("Employee was not found");
     }
 
-    @Test void testFindEmployeeBySsn_Success() {
+    @Test void testFindEmployeeByEmail_Success() {
         // Arrange
-        when(employeeCredentialsRepository.findEmployeeByEmail(testEmployee2Credentials.getEmail())).thenReturn(Optional.of(testEmployee2Credentials));
-        when(userMapper.toUserModel(testEmployee2Credentials)).thenReturn(testEmployeeModel2);
+        when(employeeCredentialsRepository.findEmployeeByEmail(testEmployee2.getEmail())).thenReturn(Optional.of(testEmployee2));
+        when(userMapper.toUserModel(testEmployee2)).thenReturn(testEmployeeModel2);
 
         // Act
-        UserCredentialsModel actualEmployee = employeeService.findEmployeeByEmail(testEmployee2Credentials.getEmail());
+        UserCredentialsModel actualEmployee = employeeService.findEmployeeByEmail(testEmployee2.getEmail());
 
         // Verify & Assert
         verify(employeeCredentialsRepository).findEmployeeByEmail(any(String.class));
@@ -105,7 +107,7 @@ public class UserCredentialsServiceTest {
         assertThat(actualEmployee).usingRecursiveComparison().isEqualTo(testEmployeeModel2);
     }
 
-    @Test void testFindEmployeeBySsn_NotFound() {
+    @Test void testFindEmployeeByEmail_NotFound() {
         // Arrange
         when(employeeCredentialsRepository.findEmployeeByEmail("xyz")).thenReturn(Optional.empty());
 
@@ -117,8 +119,8 @@ public class UserCredentialsServiceTest {
 
     @Test void testAddEmployee_Success() {
         // Arrange
-        when(userMapper.toEmployee(testEmployeeModel1)).thenReturn(testEmployee1Credentials);
-        when(employeeCredentialsRepository.existsById(testEmployee1Credentials.getId())).thenReturn(false);
+        when(userMapper.toEmployee(testEmployeeModel1)).thenReturn(testEmployee1);
+        when(employeeCredentialsRepository.existsById(testEmployee1.getId())).thenReturn(false);
 
         // Act
         employeeService.addNewEmployee(testEmployeeModel1);
@@ -131,8 +133,8 @@ public class UserCredentialsServiceTest {
 
     @Test void testAddEmployee_AlreadyExists() {
         // Arrange
-        when(userMapper.toEmployee(testEmployeeModel1)).thenReturn(testEmployee1Credentials);
-        when(employeeCredentialsRepository.existsById(testEmployee1Credentials.getId())).thenReturn(true);
+        when(userMapper.toEmployee(testEmployeeModel1)).thenReturn(testEmployee1);
+        when(employeeCredentialsRepository.existsById(testEmployee1.getId())).thenReturn(true);
 
         // Act & Assert
         assertThatThrownBy(() -> employeeService.addNewEmployee(testEmployeeModel1))
@@ -145,14 +147,14 @@ public class UserCredentialsServiceTest {
 
     @Test void deleteEmployee_Success() {
         // Arrange
-        when(employeeCredentialsRepository.findById(testEmployee2Credentials.getId())).thenReturn(Optional.of(testEmployee2Credentials));
+        when(employeeCredentialsRepository.findById(testEmployee2.getId())).thenReturn(Optional.of(testEmployee2));
 
         // Act
-        employeeService.deleteEmployee(testEmployee2Credentials.getId());
+        employeeService.deleteEmployee(testEmployee2.getId());
 
         // Verify & Assert
-        verify(employeeCredentialsRepository).findById(testEmployee2Credentials.getId());
-        verify(employeeCredentialsRepository).delete(testEmployee2Credentials);
+        verify(employeeCredentialsRepository).findById(testEmployee2.getId());
+        verify(employeeCredentialsRepository).delete(testEmployee2);
     }
 
     @Test void deleteEmployee_NotFound() {
@@ -170,27 +172,58 @@ public class UserCredentialsServiceTest {
 
     @Test void updateEmployee_Success() {
         // Arrange
-        UserCredentialsModel updatedEmployeeModel = new UserCredentialsModel("53", "543-93-5322", "Laury", "Parker", "laury.parker@example.com", "newPassword321");
-        when(employeeCredentialsRepository.findById(testEmployee2Credentials.getId())).thenReturn(Optional.of(testEmployee2Credentials));
+        UserCredentialsModel updatedEmployeeModel = new UserCredentialsModel(
+                "53",
+                "543-93-5322",
+                "Laury",
+                "Parker",
+                "laury.parker@example.com",
+                "newPassword321");
+        when(employeeCredentialsRepository.findById(testEmployee2.getId())).thenReturn(Optional.of(testEmployee2));
 
         // Act
-        employeeService.updateEmployee(testEmployee2Credentials.getId(), updatedEmployeeModel);
+        employeeService.updateEmployee(testEmployee2.getId(), updatedEmployeeModel);
 
         // Verify & Assert
         verify(employeeCredentialsRepository).findById(any(String.class));
         verify(employeeCredentialsRepository).save(any(EmployeeCredentials.class));
-        assertThat(testEmployee2Credentials.getPassword()).isEqualTo(updatedEmployeeModel.getPassword());
+        assertThat(testEmployee2.getPassword()).isEqualTo(updatedEmployeeModel.getPassword());
     }
 
     @Test void updateEmployee_NotFound() {
         // Arrange
-        UserCredentialsModel updatedEmployeeModel = new UserCredentialsModel("xyz", "543-93-5322", "Laury", "Parker", "laury.parker@example.com", "newPassword321");
+        UserCredentialsModel updatedEmployeeModel = new UserCredentialsModel(
+                "xyz",
+                "543-93-5322",
+                "Laury",
+                "Parker",
+                "laury.parker@example.com",
+                "newPassword321");
         when(employeeCredentialsRepository.findById("xyz")).thenReturn(Optional.empty());
 
         // Act & Assert
         assertThatThrownBy(() -> employeeService.updateEmployee("xyz", updatedEmployeeModel))
                 .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("Employee was not found");
+
+        // Verify No interaction
+        verify(employeeCredentialsRepository, never()).save(any(EmployeeCredentials.class));
+    }
+
+    @Test void updateEmployee_IdPathRequestMismatch() {
+        // Arrange
+        UserCredentialsModel updatedEmployeeModel = new UserCredentialsModel(
+                "xyz",
+                "543-93-5322",
+                "Laury",
+                "Parker",
+                "laury.parker@example.com",
+                "newPassword321");
+
+        // Act & Assert
+        assertThatThrownBy(() -> employeeService.updateEmployee("53", updatedEmployeeModel))
+                .isInstanceOf(ValidationException.class)
+                .hasMessage("Path ID and Request ID not matching");
 
         // Verify No interaction
         verify(employeeCredentialsRepository, never()).save(any(EmployeeCredentials.class));
